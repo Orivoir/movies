34f94fd4641528fa73e973dd6135d67532369fd7
S.Gaborieau <sam.gabor@hotmail.com>
2019-10-11 19:19:04 +0200

    define min structure directory

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import Home from './pages/Home/Home';
+
+const routes = [
+    {
+        id: 0
+        ,exact: true
+        ,path: '/'
+        ,name: 'home'
+        ,component: Home
+    }
+] ;
+
+export default routes;
