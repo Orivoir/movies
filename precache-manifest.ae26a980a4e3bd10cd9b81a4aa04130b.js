bc9b2512306518b7dee89ee88ea8da786d2e42cc
= <=>
2019-10-13 01:16:36 +0200

    Updates

---
+++ precache-manifest.ae26a980a4e3bd10cd9b81a4aa04130b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292afacd36b5c9af02682a5c18efc31c",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "26ae29fc3bbf4f6f9a80",
+    "url": "/movies/static/css/main.b94225a7.chunk.css"
+  },
+  {
+    "revision": "33f943fa1ec93259603f",
+    "url": "/movies/static/js/2.fc65ab47.chunk.js"
+  },
+  {
+    "revision": "26ae29fc3bbf4f6f9a80",
+    "url": "/movies/static/js/main.c81c697d.chunk.js"
+  },
+  {
+    "revision": "78dbb36ae65c53bcc8f1",
+    "url": "/movies/static/js/runtime-main.83833e2a.js"
+  }
+]);
